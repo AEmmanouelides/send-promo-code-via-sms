eef2d40e00f6d6386a53a25394140486fada7d96
AlexTopgun7 <aemmanouelides@hotmail.com>
2020-01-08 23:44:33 +0200

    Some minor changes/Simple CSS styling

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export function App() {
     const [over18, setOver18] = useState(false);
     const [acceptedTerms, setAcceptedTerms] = useState(false);
 
-    const handleSubmit = (evt) => {
-      evt.preventDefault();
+    const handleSubmit = (event) => {
+      event.preventDefault();
       axios.post(`/api/sms-promotion`, { phoneNumber }, { timeout: 5000 })
       .then((response) => {
         setResponseText(response.data)
@@ -23,6 +23,7 @@ export function App() {
       <div>
         <header className="App-header">
           <form onSubmit={handleSubmit}>
+          <center><h3>Send Promotional Code Via SMS using Twilio</h3></center>
             <label>Please enter your phone number: </label>
             <input
               id="phoneNumber"
@@ -34,21 +35,21 @@ export function App() {
               onChange={e => setPhoneNumber(e.target.value)}
             /><br/>
             <input 
-            type="checkbox" 
-            name="over18" 
-            required 
-            value={over18} 
-            onChange={e => setOver18(e.target.checked)}
+              type="checkbox" 
+              name="over18" 
+              required 
+              value={over18} 
+              onChange={e => setOver18(e.target.checked)}
             />
-            I am over 18 <br/>
+            <label>I am over 18</label><br/>
             <input 
-            type="checkbox" 
-            name="acceptedTerms" 
-            required 
-            value={acceptedTerms} 
-            onChange={e => setAcceptedTerms(e.target.checked)}
+              type="checkbox" 
+              name="acceptedTerms" 
+              required 
+              value={acceptedTerms} 
+              onChange={e => setAcceptedTerms(e.target.checked)}
             />
-            I accept the terms and conditions <br/>
+            <label>I accept the terms and conditions</label> <br/> <br/>
             <button type="submit">Submit</button>
           </form>
           <p>{responseText}</p>
